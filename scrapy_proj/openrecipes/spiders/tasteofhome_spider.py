2dc0560c5b54d0810c5a35b0e331fef7bb0080c2
Rich Kroll <kroll.rich@gmail.com>
2013-10-13 12:53:02 -0400

    Taste of home spider & tests

---
+++ scrapy_proj/openrecipes/spiders/tasteofhome_spider.py
@@ -0,0 +1,87 @@
+from scrapy.contrib.spiders import CrawlSpider, Rule
+from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
+from scrapy.selector import HtmlXPathSelector
+from openrecipes.items import RecipeItem, RecipeItemLoader
+from openrecipes.util import flatten
+
+
+class TasteofhomeMixin(object):
+  source = 'tasteofhome'
+
+  def _parse(self, root, schema, data={}):
+    rootStr = root.extract()
+    attrMap = {
+      'photo': '@src',
+      'image': '@src',
+      'url': '@href',
+      'prepTime': '@content',
+      'cookTime': '@content',
+      'totalTime': '@content',
+      'datePublished': '@content',
+      }
+    data['itemtype'] = schema
+    props = root.select('.//*[@itemprop]')
+
+    for prop in props:
+      node = prop
+      skip = False
+      name = prop.select('@itemprop').extract()[0]
+      prevValue = data.get(name, None)
+
+      while True:
+        if node.extract() == rootStr:
+          break
+        if node.select('@itemscope'):
+          skip = True
+          break
+
+        node = node.select('parent::*')[0]
+
+      if skip:
+        continue
+
+      if prop.select('@itemscope'):
+        value = _parse(prop, prop.select('@itemtype').extract()[0])
+      else:
+        value = [''.join(prop.select(attrMap.get(name, ".//text()[normalize-space()]")).extract()).strip()]
+
+      if prevValue is None:
+        data[name] = value
+      else:
+        prevValue.extend(value)
+
+    return data
+
+
+  def parse_recipes(self, scope, data={}):
+    schema = 'http://schema.org/recipe'
+    recipes = [self._parse(recipe, schema, data) for recipe in scope.select('//*[@itemtype="%s"]' % schema)]
+
+    return recipes
+
+  def parse_item(self, response):
+    hxs = HtmlXPathSelector(response)
+    raw_recipes = self.parse_recipes(hxs, {'source': self.source, 'url': response.url})
+    for recipe in raw_recipes:
+      if 'photo' in recipe:
+        recipe['photo'] = flatten(recipe['photo'])
+      if 'image' in recipe:
+        recipe['image'] = flatten(recipe['image'])
+
+    return [RecipeItem.from_dict(recipe) for recipe in raw_recipes]
+
+
+class TasteofhomecrawlSpider(CrawlSpider, TasteofhomeMixin):
+
+  name = "tasteofhome.com"
+
+  allowed_domains = ["www.tasteofhome.com"]
+
+  start_urls = [
+    'http://www.tasteofhome.com/recipes/',
+    ]
+
+  rules = (
+    Rule(SgmlLinkExtractor(allow=('/recipe/.+')),
+         callback='parse_item'),
+  )
